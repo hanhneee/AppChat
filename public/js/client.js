c3a4620fbdc7f9d4c3d15a5b888246e1b27ce034
hanhneee <hanhminhvo1809@gmail.com>
2025-03-22 23:21:08 +0700

    second commit

--- public/js/client.js
+++ public/js/client.js
@@ -1,30 +1,58 @@
-const ip_name =document.getElementById("name")
-const ip_room =document.getElementById("room")
-const btn_join =document.getElementById("btn_join")
+const ip_name = document.getElementById("name")
+const ip_room = document.getElementById("room")
+const btn_join = document.getElementById("btn_join")
 
-const ip_message =document.getElementById("ip_message")
-const btn_send =document.getElementById("btn_send")
+const ip_message = document.getElementById("ip_message")
+const btn_send = document.getElementById("btn_send")
 const ul_message = document.getElementById("ul_message")
 
-var socket =io.connect()
+var socket = io.connect()
 
-socket.on("connect", function(data){
+let my_name = ""
+
+socket.on("connect", function (data) {
     console.log(data);
 })
 
-btn_join.addEventListener('click',()=>{
-    const room =ip_room.value
-    socket.emit("join",room)
+btn_join.addEventListener('click', () => {
+    const room = ip_room.value
+    my_name = ip_name.value
+    socket.emit("join", room)
+    alert('Vao phong thanh cong')
 })
-btn_send.addEventListener('click', ()=>{
-    const message = ip_message.value
-    const name = ip_name.value
-    socket.emit("message",name + ": " +message)
 
+
+const sendMessage = () => {
+    const message = ip_message.value
+    if(!message){
+        return;
+    }
+    const obj = {
+        name: my_name,
+        message: message
+    }
+    socket.emit("message", JSON.stringify(obj));
+    ip_message.value='';
+    ip_message.focus();
+}
+
+btn_send.addEventListener('click',sendMessage )
+
+ip_message.addEventListener('keydow',(event)=>{
+    if(event.key ==='Enter'){
+        sendMessage();
+    }
 })
 
 socket.on("thread", function (data) {
+    const obj = JSON.parse(data)
+
     const li = document.createElement("li")
-    li.innerHTML = data
+    li.innerHTML = obj.message;
+
+    if(obj.name === my_name){
+        li.classList.add("right")
+    }
+
     ul_message.appendChild(li)
 })
