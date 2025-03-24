91d35c3b8dac79bc82d6c178dea304b2243da1ce
ductrong23 <trong9dtk@gmail.com>
2025-03-24 15:37:28 +0700

    route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const connectDB = require("./configs/database");
+const router = require("./routers");
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -27,9 +28,11 @@ io.on("connection", function (client) {
 
 connectDB();
 
-app.get("/chat", (req, res) => {
-  return res.render("chat.ejs");
-});
+router(app);
+
+// app.get("/chat", (req, res) => {
+//   return res.render("chat.ejs");
+// });
 
 server.listen(5000, () => {
   console.log("Server is running on port 5000");
---
+++ routers/account.router.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const { register, login } = require("../controllers/account.controller");
+
+router
+.route("/login")
+.post(login)
+
+router
+.route("/register")
+.post(register);
+
+module.exports=router
