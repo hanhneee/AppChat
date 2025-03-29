34a1d6c088f8eb42c3c687883d53ec09b5c90c55
NamTruong-LN <gitblue8469@gmail.com>
2025-03-29 10:13:12 +0700

    css new

--- public/js/client.js
+++ public/js/client.js
@@ -10,22 +10,47 @@ var socket = io.connect()
 
 let my_name = localStorage.getItem("username");
 
-socket.on("connect", function(data){
-   console.log(data)
+const emotions = [
+    {
+        id: 1,
+        emotion: '<i class="fa-solid fa-heart" style="color: #ea0606;"></i>'
+    },
+    {
+        id: 2,
+        emotion: '<i class="fa-solid fa-face-grin-tears" style="color: #abd31d;"></i>'
+    },
+    {
+        id: 3,
+        emotion: '<i class="fa-solid fa-face-sad-tear" style="color: #e8c854;"></i>'
+    },
+    {
+        id: 4,
+        emotion: '<i class="fa-solid fa-face-angry" style="color: #d46911;"></i>'
+    }
+]
+
+socket.on("connect", function (data) {
+    console.log(data)
 })
 
-btn_join.addEventListener("click",()=>{
+btn_join.addEventListener("click", () => {
     const room = ip_room.value
     socket.emit("join", room)
     alert(`Join room ${room} thành công *()*`)
 })
 
-const sendMessage = ()=>{
+const sendMessage = () => {
     const message = ip_message.value
-    if(!message){
+    if (!message) {
         return;
     }
+
+    let id = ''
+    for(let i = 0; i <8 ; i++  ){
+        id += Math.floor(Math.random() * 10)
+    }
     const obj = {
+        id: +id,
         name: my_name,
         message: message
     }
@@ -37,21 +62,84 @@ const sendMessage = ()=>{
 btn_send.addEventListener("click", sendMessage)
 
 
-ip_message.addEventListener('keydown',(event)=>{
-    if(event.key === "Enter"){
+ip_message.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
         sendMessage()
     }
 })
 
-socket.on("thread", function(data){
+socket.on("thread", function (data) {
     const obj = JSON.parse(data)
 
     const li = document.createElement("li")
-    li.innerHTML = obj.message;
+    li.innerHTML = `
+        <span id = "${obj.id}">
+            <p>${obj.message}</p>
 
-    if(obj.name === my_name){
+        </span>
+        <i onclick="show(event,${obj.id})" class="choose_emotion fa-solid fa-face-smile-beam"></i>
+            
+    `;
+
+    if (obj.name === my_name) {
         li.classList.add("right");
     }
     ul_message.appendChild(li)
     ul_message.scrollTop = ul_message.scrollHeight;
 })
+
+function show(e,id) {
+    if (e.target.classList.contains("choose_emotion")) {
+        if (e.target.innerHTML.toString().trim().length === 0) {
+            e.target.innerHTML = `
+        <div class="emtions">
+            <i onclick="choose(event,${id}, 1)" class="fa-solid fa-heart" style="color: #ea0606;"></i>
+            <i onclick="choose(event,${id}, 2)" class="fa-solid fa-face-grin-tears" style="color: #abd31d;"></i>
+            <i onclick="choose(event,${id}, 3)" class="fa-solid fa-face-sad-tear" style="color: #e8c854;"></i>
+            <i onclick="choose(event,${id}, 4)" class="fa-solid fa-face-angry" style="color: #d46911;"></i>
+        </div>
+    `
+        } else {
+            e.target.innerHTML = '';
+        }
+    }
+}
+
+function choose(e,id, id_emotion){
+    const span_message = document.getElementById(id + "");
+  
+    const emotion = e.target;
+    emotion.style.position = 'absolute'
+    emotion.style.top= '27px',
+    emotion.style.left= '5px'
+    emotion.style.backgroundColor = 'blue'
+    emotion.style.borderRadius = '50%'
+
+    span_message.appendChild(emotion)
+
+    const obj = {
+        id: id,
+        emotion: id_emotion
+    }
+    socket.emit("emotion", JSON.stringify(obj));
+}
+
+socket.on("emotion",(data)=> {
+    const obj = JSON.parse(data);
+    const span_message = document.getElementById(obj.id + "");
+  
+    let emotion = emotions[obj.emotion - 1].emotion;
+    const div = document.createElement("div")
+    div.innerHTML = emotion 
+    emotion = div.firstChild;
+
+    emotion.style.position = 'absolute'
+    emotion.style.top= '27px',
+    emotion.style.left= '5px'
+    emotion.style.backgroundColor = 'blue'
+    emotion.style.borderRadius = '50%'
+
+    span_message.appendChild(emotion)
+
+})
+
